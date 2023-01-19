86a922edb70659f21677b319fa1a7797c4014d4c
Doppler344 <Doppler344@yandex.ru>
2023-01-19 22:15:41 +0300

    env fixed

--- stopwait/settings.py
+++ stopwait/settings.py
@@ -18,6 +18,7 @@ env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
 )
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
